88e0dd8816c85ccaae1c0e85875b0d7ba77b5b0e
Pamela Hartwig <pamela929@gmail.com>
2023-05-26 16:26:01 +0200

    Add x86 arch arm

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -22,9 +22,13 @@ class ListsController < ApplicationController
     end
   end
 
+  # def destroy
+  #   @list.destroy
+  #   redirect_to lists_path, status: :see_other
+  # end
   # prive
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :photo)
   end
 end
